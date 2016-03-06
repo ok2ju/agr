ede4e8179037e67e0a29360774033767604ae055
ok2ju <soulSpringg@gmail.com>
2016-03-06 15:25:59 +0300

    fix sections, and slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,41 @@
 $(document).ready(function() {
+
   $(".Modern-Slider").slick({
-    autoplay:false,
-    autoplaySpeed:10000,
-    speed:600,
-    slidesToShow:1,
-    slidesToScroll:1,
-    pauseOnHover:false,
+    autoplay: true,
+    autoplaySpeed: 10000,
+    speed: 600,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    pauseOnHover: false,
     dots: true,
-    pauseOnDotsHover:true,
+    pauseOnDotsHover: true,
     cssEase:'linear',
    // fade:true,
-    draggable:false,
+    draggable: false,
     arrows: false
   });
+
+  var $slideshow = $(".events-slider");
+
+  $('.slider-nav-list').on('click', 'a', function(e) {
+    e.preventDefault();
+    slideIndex = $(this).closest('li').index();
+    $slideshow.slick('slickGoTo', slideIndex, false);
+  });
+
+  $slideshow.slick({
+    autoplay: false,
+    autoplaySpeed: 10000,
+    speed: 600,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    pauseOnHover: false,
+    dots: false,
+    pauseOnDotsHover: true,
+    cssEase:'linear',
+    fade:true,
+    draggable: false,
+    arrows: false
+  });
+
 });
